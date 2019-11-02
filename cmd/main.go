28c3face7c15d0414e35faf8b745e26c83abfc01
radabaugh <radabaughtyler24@gmail.com>
2019-11-02 15:29:51 -0700

    fix css

--- cmd/main.go
+++ cmd/main.go
@@ -19,7 +19,7 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("cmd/templates/*.tmpl.html")
-	router.Static("/static", "static")
+	router.Static("cmd/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
