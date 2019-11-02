3cf67519ab0baff77d9b5071f836d9a6022fbad8
radabaugh <radabaughtyler24@gmail.com>
2019-11-02 15:20:27 -0700

    moved procfile

--- cmd/main.go
+++ cmd/main.go
@@ -25,5 +25,13 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
+	router.GET("/sources", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "sources.tmpl.html", nil)
+	})
+
+	router.GET("/destinations", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "destinations.tmpl.html", nil)
+	})
+
 	router.Run(":" + port)
 }
